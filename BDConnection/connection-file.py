19cec719ec16e8b72e946d6ec973dc7fc7d1948a
xXBtKaos <manuelcortina2018@gmail.com>
2023-03-21 21:47:59 +0100

    Added env working functionality. Changed BD Connection mode (need review)

--- BDConnection/connection-file.py
+++ BDConnection/connection-file.py
@@ -1,10 +1,15 @@
 import pyodbc
+import pandas as pd
 
-server = 'tcp.myserver.database.windows.net'
-database = 'mydb'
+server = 'localhost'
+database = 'TurboRenting2'
 username = 'sa'
 password = 'test1234'
 driver = 'ODBC Driver 18 for SQL Server'
 
-con = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};ENCRYPT=yes;UID={username};PwD={password}')
-cursor = con.cursor()
+conn = pyodbc.connect('driver={%s};server=%s;databse=%s;uid=%s;pwd=%s' % (driver, server, database, username, password))
+
+# Query
+
+res = pd.read_sql("SELECT * FROM dbo.Clients c WHERE c.Dni like '71725443N'", con = conn)
+res
